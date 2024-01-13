0cad4c36587b9274f88dfad437841a671cc8bc7c
Tristan Naess <tristannaess@gmail.com>
2024-01-13 10:13:24 -0800

    Restarted Day 16, File processing into structures implemented

--- Day16/main.cpp
+++ Day16/main.cpp
@@ -7,35 +7,78 @@
 class Grid
 {
     public:
-        Grid(const std::vector<std::string>& data);
-
-        std::size_t activate()
+        Grid(const std::vector<std::string>& data)
         {
-            // add beam{0,0,Right} to queue
-            // for each beam check cell, make hot, close paths, add next step to queue
+            m_height = data.size();
+            m_width = data.front().size();
 
-            // return number of hot cells
-            return 0;
+            m_data.reserve(m_height * m_width);
+
+            for (const auto& row : data)
+            {
+                for (const char& c : row)
+                {
+                    m_data.emplace_back(c);
+                }
+            }
         }
 
     private:
+
+        enum class Direction : unsigned char { Up, Down, Left, Right };
+
         struct Cell
         {
+            Cell(const char c)
+            {
+                hot = false;
+                switch (c)
+                {
+                    case '.':  type = Type::Empty;   break;
+                    case '/':  type = Type::RMirror; break;
+                    case '\\': type = Type::LMirror; break;
+                    case '|':  type = Type::VSplit;  break;
+                    case '-':  type = Type::HSplit;  break;
+                }
+            }
+
+            enum class Type { Empty, RMirror, LMirror, VSplit, HSplit } type;
             bool hot;
-            // See if there's a better way to store directions, that can be reused in Beam
-            struct Dirs { bool Up = false, Down = false, Left = false, Right = false; };
-
-            Dirs closed_paths;
+            Direction accessed;
         };
 
-        std::vector<Cell> cells;
+        std::size_t m_width, m_height;
+        std::vector<Cell> m_data;
 
-        struct Beam
-        {
-            std::size_t x, y;
-            // store direction somehow
-        };
+        friend std::ostream& operator<<(std::ostream& os, const Grid& g);
 };
+
+std::ostream& operator<<(std::ostream& os, const Grid& g)
+{
+    std::size_t count = 0;
+    for (const Grid::Cell& c : g.m_data)
+    {
+        if (c.hot) os << "\033[7m";
+
+        switch (c.type)
+        {
+            case Grid::Cell::Type::Empty:   os << '.';  break;
+            case Grid::Cell::Type::RMirror: os << '/';  break;
+            case Grid::Cell::Type::LMirror: os << '\\'; break;
+            case Grid::Cell::Type::VSplit:  os << '|';  break;
+            case Grid::Cell::Type::HSplit:  os << '-';  break;
+        }
+        os << "\033[0m";
+
+        count++;
+        if (count == g.m_width)
+        {
+            os << '\n';
+            count = 0;
+        }
+    }
+    return os;
+}
 
 int main(int argc, char** argv)
 {
@@ -59,5 +102,9 @@ int main(int argc, char** argv)
     {
         data.push_back(buffer);
     }
+
+    Grid grid(data);
+
+    std::cout << grid << '\n';
 
 }
